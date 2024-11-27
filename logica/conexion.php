7b1ed7f92ce9a5a6ff4cc28e4679d4a26918c9a9
AlnOtzar”git config --global user.email [200~aotzar2003@gmail.com” <aotzar2003@gmail.com>
2024-11-26 20:32:06 -0600

    Actuallizacion de credenciales linux

--- logica/conexion.php
+++ logica/conexion.php
@@ -1,9 +1,25 @@
 <?php
-// esta pestaña es para conectarnos a la base de datos
-$host_db = "localhost:3306";
-$user_db = "root";
-$pass_db = "ImpliedFiber356@";
-$db_name = "Disquera";
+// LOCAL
+
+// $host_db = "localhost:3306";
+// $user_db = "root";
+// $pass_db = "ImpliedFiber356@";
+// $db_name = "Disquera";
+
+
+// InfinityFree, la contrasena es la de SQL EN INFINITYFREE
+
+// $host_db = "sql205.infinityfree.com";
+// $user_db = "if0_37329493";
+// $pass_db = "v9k69MJUbft";
+// $db_name = "if0_37329493_fes_aragon";
+
+//SERVIDOR LINUX
+$servername = 'localhost:3306';
+$username = 'alanotzarwav';
+$password = 'ImpliedFiber356@';
+$dbname = 'Disquera';
+
 
 
 $conexion = new mysqli($host_db,$user_db,$pass_db,$db_name);
